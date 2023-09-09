5f9b45a724e61221bc597f2ccdce0388e11f0112
oboard <oboard@outlook.com>
2023-09-09 23:00:55 +0800

    init

---
+++ app/xhs/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function XHS() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1 className="text-4xl font-bold">小红书</h1>
+      </main>
+    </>
+  );
+}
